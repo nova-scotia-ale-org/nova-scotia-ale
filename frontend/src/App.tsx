82c25f929db125500579385fa88261f30aa3921c
TanujDonkal <tanujdonkal29@gmail.com>
2026-02-17 16:44:34 -0400

    chore: initial frontend setup with Vite + Bootstrap (closes #1)

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1 className="text-primary">Nova Scotia Ale 🍺</h1>
+      <button className="btn btn-success mt-3">
+        Explore Beers
+      </button>
+    </div>
+  );
+}
+
+export default App;
+
+
