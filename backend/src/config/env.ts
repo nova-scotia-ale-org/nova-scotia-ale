fc8ba8c605cc494fc0d7c3d63be8ac595e6c186f
TanujDonkal <tanujdonkal29@gmail.com>
2026-02-23 17:33:14 -0400

    feat(auth): implement register and login APIs using Supabase (closes #23)

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -1,18 +1,10 @@
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const requiredEnvVars = ["SUPABASE_URL", "SUPABASE_ANON_KEY"];
-
-requiredEnvVars.forEach((key) => {
-  if (!process.env[key]) {
-    throw new Error(`Missing required environment variable: ${key}`);
-  }
-});
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const env = {
-  port: process.env.PORT || "5000",
-  supabaseUrl: process.env.SUPABASE_URL as string,
-  supabaseAnonKey: process.env.SUPABASE_ANON_KEY as string,
-  nodeEnv: process.env.NODE_ENV || "development",
-};
+  PORT: Number(process.env.PORT) || 5000,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+
+  SUPABASE_URL: process.env.SUPABASE_URL!,
+  SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY!
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { env } from "./config/env.js";
+import authRoutes from './routes/auth.routes.js'
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes)
 
 app.get("/health", (_req, res) => {
   res.status(200).json({ status: "OK", message: "Backend is running 🚀" });
@@ -16,6 +18,7 @@ app.get("/health", (_req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(env.port, () => {
-  console.log(`🚀 Server running on port ${env.port}`);
+app.use('/api/auth', authRoutes)
+app.listen(env.PORT, () => {
+  console.log(`🚀 Server running on port ${env.PORT}`);
 });
