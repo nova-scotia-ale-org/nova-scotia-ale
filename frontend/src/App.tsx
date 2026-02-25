5c16e6d501906094dea7c640404def43fa61c27a
Sanjeev Sharma <sanjeevsharma98103@gmail.com>
2026-02-25 15:38:06 -0400

    fix(test): allow multiple Nova Scotia Ale text matches

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,6 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
